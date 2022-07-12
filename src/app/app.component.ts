cc2481880df617c0a0b0f0b2a5318cbba8f5889f
Dasarro <wicia0202@wp.pl>
2022-07-12 09:12:08 +0200

    Create quotes simulation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StockPrice } from 'src/models/stockPrice';
+import { stockPricesData } from 'src/assets/stockPrices';
+import { of } from 'rxjs';
+import { delay, concatWith } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ask-and-bid';
+export class AppComponent implements OnInit {
+  currentStockPrice: StockPrice | null = null;
+
+  private getStockPrices(): void {
+    const delays = stockPricesData
+      .map(stockPrice => stockPrice.datetime)
+      .map((datetime, i, array) => {
+        if (i === 0) return 0;
+        return datetime - array[i-1];
+      });
+
+    const observables = stockPricesData.map((stockPrice, i) => of(stockPrice).pipe(delay(delays[i])));
+    const observable = observables[0].pipe(concatWith(...observables.slice(1)));
+    observable.subscribe(stockPrice => this.currentStockPrice = stockPrice);
+  }
+
+  formatDate(datetime: number): string {
+    return moment(datetime).format('YYYY-MM-DD HH:mm:ss.SSS');
+  }
+
+  ngOnInit(): void {
+    this.getStockPrices();
+  }
 }
---
+++ src/models/stockPrice.ts
@@ -0,0 +1,7 @@
+export interface StockPrice {
+    ask: number,
+    ask_size: number,
+    bid: number,
+    bid_size: number,
+    datetime: number
+}
